86d4e5fad8b0ecf19eb5c3ad1ca5844867c6f261
Maximum Hallinan <maxhallinan@gmail.com>
2017-04-10 16:08:05 -0400

    Add tests for unit > ui > buildPrompt

--- test/unit/ui.js
+++ test/unit/ui.js
@@ -290,7 +290,59 @@ describe('buildSelected', function () {
   });
 });
 
-describe('buildPrompt', function () {});
+describe('buildPrompt', function () {
+  const choices = [
+    {
+      name: 'foo',
+    },
+    {
+      name: 'bar',
+    },
+    {
+      name: 'baz',
+    },
+  ];
+
+  const selected = [ 'foo', 'bar', ];
+
+  let output;
+
+  before(function () {
+    output = ui.buildPrompt({ choices, selected, });
+  });
+
+  it('Returns an object with the expected shape.', function () {
+    const outputKeys = Object.keys(output);
+
+    const expected = [ 'choices', 'default', 'message', 'name', 'pageSize', 'type', ];
+
+    assert.includeMembers(outputKeys, expected);
+  });
+
+  it('Sets the expected choices.', function () {
+    assert.deepEqual(output.choices, choices);
+  });
+
+  it('Sets the expected default.', function () {
+    assert.deepEqual(output.default, selected);
+  });
+
+  it('Sets the expected message.', function () {
+    assert.strictEqual(output.message, 'Select branches to delete: ');
+  });
+
+  it('Sets the expected name.', function () {
+    assert.strictEqual(output.name, 'branches');
+  });
+
+  it('Sets the expected page size.', function () {
+    assert.strictEqual(output.pageSize, 20);
+  });
+
+  it('Sets the expected type.', function () {
+    assert.strictEqual(output.type, 'checkbox');
+  });
+});
 
 describe('getAnswers', function () {});
 

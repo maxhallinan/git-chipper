37ad45dcaf4c1c6a9342d82d8390b98521ccd1bc
Maximum Hallinan <maxhallinan@gmail.com>
2017-04-10 15:40:34 -0400

    Write tests for unit > ui > buildSelected

--- test/unit/ui.js
+++ test/unit/ui.js
@@ -242,7 +242,53 @@ describe('filterSelected', function () {
   });
 });
 
-describe('buildSelected', function () {});
+describe('buildSelected', function () {
+  const choices = [
+    {
+      name: 'foo',
+    },
+    {
+      name: 'bar',
+    },
+    {
+      name: 'baz',
+    }
+  ];
+
+  const notSelected = [ 'foo', ];
+
+  let output;
+
+  before(function () {
+    output = ui.buildSelected(notSelected, choices);
+  });
+
+  it('Should return an object with the expected shape.', function () {
+    const outputKeys = Object.keys(output);
+
+    const expected = [ 'choices', 'selected', ];
+
+    assert.includeMembers(outputKeys, expected);
+    assert.strictEqual(outputKeys.length, 2);
+  });
+
+  it('Should set `choices` to the second argument', function () {
+    assert.deepEqual(output.choices, choices);
+  });
+
+  it('Should set `selected` to an empty array if notSelected is empty.', function () {
+    const choices = [];
+    const notSelected = [];
+
+    assert.deepEqual(ui.buildSelected(notSelected, choices).selected, []);
+  });
+
+  it('Should set `selected` to an array of choice names if notSelected is not empty', function () {
+    const expected = [ 'bar', 'baz', ];
+
+    assert.deepEqual(output.selected, expected);
+  });
+});
 
 describe('buildPrompt', function () {});
 

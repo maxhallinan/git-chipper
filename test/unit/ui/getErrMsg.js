ab54dbe69c24d8dbeaf8baf2d004d1fb1b24105a
Maximum Hallinan <maxhallinan@gmail.com>
2017-04-10 21:50:35 -0400

    Write tests for getErrMsg

--- test/unit/ui/getErrMsg.js
+++ test/unit/ui/getErrMsg.js
@@ -1,5 +1,21 @@
 const assert = require('chai').assert;
 const ui = require('../../../ui');
 
-describe('unit > ui > getErrMsg', function () {});
+describe('unit > ui > getErrMsg', function () {
+  it('Returns the message of an error object', function () {
+    const err = { message: 'foo', };
+
+    const output = ui.getErrMsg(err);
+
+    assert.strictEqual(output, err.message);
+  });
+
+  it('Falls back to returning the error value if there is no error.message', function () {
+    const err = 'foo';
+
+    const output = ui.getErrMsg(err);
+
+    assert.strictEqual(output, err);
+  });
+});
 
